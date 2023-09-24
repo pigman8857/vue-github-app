97ec98db59d8cffb08e4a5c379d8d41db35aca23
SomkamolV <somkamolv@hotmail.com>
2023-09-24 15:58:28 +0700

    use Serhiika/VuePagesAction@1.0.4

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Vue APP!" />
+      <HelloWorld msg="Vue APP! v2.0.0" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
